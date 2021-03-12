5f5a10516e5bc6c135b83c0f6dec792e75011288
Kiana Long <kianajade42.kl@gmail.com>
2021-03-12 21:36:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
-
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
 class App < Sinatra::Base
+  get '/' do 
+    erb :index
+  end
 end
